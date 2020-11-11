7391145342d2aa65c9593303af21728defb2e388
Sepehr Fazeli <sky.f1118@gmail.com>
2020-11-11 12:54:32 +0330

    Message Generator v 1.0

--- js/projects/MixedMessage/MixedMessage.js
+++ js/projects/MixedMessage/MixedMessage.js
@@ -1,2 +1,22 @@
 //Fortune Car
-console.log("**Fortune Car**")
+
+const Stored_Messages = {
+  type: ["brand new", "old", "rusty"],
+  color: ["black", "white", "red", "silver"],
+  cars: ["Benz", "corvette", "BMW", "dodge"],
+  randomtype() {
+    return this.type[Math.floor(Math.random() * this.type.length)];
+  },
+  randomcolor() {
+    return this.color[Math.floor(Math.random() * this.color.length)];
+  },
+  randomcars() {
+    return this.cars[Math.floor(Math.random() * this.cars.length)];
+  },
+};
+const FurtuneTeller = () => {
+    console.log(`\nYour Furtune Car is:\n** A ${Stored_Messages.randomtype()} ${Stored_Messages.randomcolor()} ${Stored_Messages.randomcars()} **\n`)
+};
+
+///////TEST///////TEST///////TEST///////TEST///////TEST///////TEST///////TEST///////TEST///////
+FurtuneTeller();
